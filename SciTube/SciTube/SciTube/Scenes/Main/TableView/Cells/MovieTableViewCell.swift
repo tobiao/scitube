5448bdddcd08d8aa2e484ecd263681b0a09b66e6
tmatczak <tobiasz.matczak@gmail.com>
2018-12-22 11:45:55 +0100

    Add basic implementation of cell with movie details.

---
+++ SciTube/SciTube/SciTube/Scenes/Main/TableView/Cells/MovieTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  MovieTableViewCell.swift
+//  SciTube
+//
+//  Created by Tobiao on 22.12.2018.
+//  Copyright © 2018 ITWhite. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    // Outlets
+    @IBOutlet weak var ivMiniature: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblDescription: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
---
+++ SciTube/SciTube/SciTube/Scenes/Main/TableView/MovieListTableViewDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  MovieListTableViewDataSource.swift
+//  SciTube
+//
+//  Created by Tobiao on 22.12.2018.
+//  Copyright © 2018 ITWhite. All rights reserved.
+//
+
+import Foundation
