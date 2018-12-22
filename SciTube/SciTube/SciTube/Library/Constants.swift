e760a155e94a968fc34b98a8cec932827633bb9d
tmatczak <tobiasz.matczak@gmail.com>
2018-12-22 11:53:51 +0100

    Add constants for colors and fonts.

---
+++ SciTube/SciTube/SciTube/Library/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  SciTube
+//
+//  Created by Tobiao on 22.12.2018.
+//  Copyright © 2018 ITWhite. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+
+    private init() { }
+    
+    static let main = UIColor.green
+    static let textGray = UIColor.darkGray
+    static let textWhite = UIColor.white
+}
+
+struct Fonts {
+
+    private init() { }
+    
+    // regular
+    static let regular14 = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight.regular)
+    static let regular16 = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.regular)
+}
